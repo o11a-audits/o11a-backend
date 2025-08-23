8836a0a7154ad0900795deac53f83343a86eeddd
jrstrunk <developer@jrstrunk.com>
2025-08-23 13:22:52 -0400

    Change process function to find json build files from project compilation output

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,6 @@ use std::path::Path;
 mod solidity;
 
 fn main() {
-    let ast_map = solidity::process(Path::new("/home/john/olla/server/priv/audits/nudgexyz"));
+    let ast_map = solidity::process(Path::new("/home/john/olla/server/priv/audits/lido-crmv2"));
     println!("Processed {} ASTs", ast_map.len());
 }
--- src/solidity/mod.rs
+++ src/solidity/mod.rs
@@ -1,8 +1,9 @@
+pub use parser::{ast_from_json_file, traverse_nodes};
 use std::collections::HashMap;
 use std::path::Path;
 
 pub mod parser;
 
-pub fn process(path: &Path) -> HashMap<i32, parser::AST> {
+pub fn process(path: &Path) -> HashMap<String, parser::AST> {
     parser::process(path)
 }
